aac976f631a93be058bae154edfd8c620679c211
Imad Mashhood <mashhoodimad@gmail.com>
2022-04-29 14:27:29 -0500

    Refactor checkbox handling. Includes UI changes and saving prev inputs

--- ADBLogger.py
+++ ADBLogger.py
@@ -1,3 +1,5 @@
+import inspect
+import json
 import os
 import sys
 import subprocess
@@ -25,6 +27,8 @@ class Ui_MainWindow(object):
     def __init__(self):
         self.t = None
         self.fileName = None
+        self.ipAddress = None
+        self.clearPrevLogs = None
 
     def setupUi(self, MainWindow):
 
@@ -34,28 +38,28 @@ class Ui_MainWindow(object):
 
         #Main Window
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(620, 280)
+        MainWindow.resize(575, 205)
         MainWindow.setAutoFillBackground(False)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
 
         #Start Button
         self.startBtn = QtWidgets.QPushButton(self.centralwidget)
-        self.startBtn.setGeometry(QtCore.QRect(510, 20, 81, 41))
+        self.startBtn.setGeometry(QtCore.QRect(480, 12, 81, 41))
         self.startBtn.setObjectName("startBtn")
         self.startBtn.clicked.connect(self.startBtnClick)
 
         #IP Label
         self.label = QtWidgets.QLabel(self.centralwidget)
-        self.label.setGeometry(QtCore.QRect(20, 30, 101, 21))
+        self.label.setGeometry(QtCore.QRect(20, 20, 101, 21))
         font = QtGui.QFont()
-        font.setPointSize(14)
+        font.setPointSize(12)
         self.label.setFont(font)
         self.label.setObjectName("label")
 
         #IP Text Box
         self.ipAddressTB = QtWidgets.QLineEdit(self.centralwidget)
-        self.ipAddressTB.setGeometry(QtCore.QRect(130, 25, 361, 35))
+        self.ipAddressTB.setGeometry(QtCore.QRect(110, 15, 361, 35))
         font = QtGui.QFont()
         font.setPointSize(12)
         self.ipAddressTB.setFont(font)
@@ -63,15 +67,15 @@ class Ui_MainWindow(object):
 
         #File Name Label
         self.label_2 = QtWidgets.QLabel(self.centralwidget)
-        self.label_2.setGeometry(QtCore.QRect(20, 90, 101, 21))
+        self.label_2.setGeometry(QtCore.QRect(20, 70, 101, 21))
         font = QtGui.QFont()
-        font.setPointSize(14)
+        font.setPointSize(12)
         self.label_2.setFont(font)
         self.label_2.setObjectName("label_2")
 
         #File Name TB
         self.fileNameTB = QtWidgets.QLineEdit(self.centralwidget)
-        self.fileNameTB.setGeometry(QtCore.QRect(130, 85, 361, 35))
+        self.fileNameTB.setGeometry(QtCore.QRect(110, 65, 361, 35))
         font = QtGui.QFont()
         font.setPointSize(12)
         self.fileNameTB.setFont(font)
@@ -79,29 +83,29 @@ class Ui_MainWindow(object):
 
         #Stop Button
         self.stopBtn = QtWidgets.QPushButton(self.centralwidget)
-        self.stopBtn.setGeometry(QtCore.QRect(510, 80, 81, 41))
+        self.stopBtn.setGeometry(QtCore.QRect(480, 62, 81, 41))
         self.stopBtn.setDisabled(True)
         self.stopBtn.setObjectName("stopBtn")
         self.stopBtn.clicked.connect(self.stopBtnClick)
 
         #Status Label
         self.status = QtWidgets.QLabel(self.centralwidget)
-        self.status.setGeometry(QtCore.QRect(130, 180, 361, 41))
+        self.status.setGeometry(QtCore.QRect(110, 130, 361, 41))
         self.status.setText("")
         self.status.setAlignment(QtCore.Qt.AlignCenter)
         self.status.setObjectName("status")
 
         #Clear Prev Logs Check Box
         self.clearPrevLogsChB = QtWidgets.QCheckBox(self.centralwidget)
-        self.clearPrevLogsChB.setGeometry(QtCore.QRect(130, 130, 161, 31))
+        self.clearPrevLogsChB.setGeometry(QtCore.QRect(110, 105, 161, 31))
         font = QtGui.QFont()
-        font.setPointSize(12)
+        font.setPointSize(10)
         self.clearPrevLogsChB.setFont(font)
         self.clearPrevLogsChB.setObjectName("clearPrevLogsChB")
 
         #IP Address Combo Box
         self.ipAddressCB = QtWidgets.QComboBox(self.centralwidget)
-        self.ipAddressCB.setGeometry(QtCore.QRect(130, 25, 361, 35))
+        self.ipAddressCB.setGeometry(QtCore.QRect(110, 15, 361, 35))
         font = QtGui.QFont()
         font.setPointSize(12)
         self.ipAddressCB.setFont(font)
@@ -111,7 +115,7 @@ class Ui_MainWindow(object):
 
         #Progress Bar
         self.progressBar = QtWidgets.QProgressBar(self.centralwidget)
-        self.progressBar.setGeometry(QtCore.QRect(130, 240, 401, 16))
+        self.progressBar.setGeometry(QtCore.QRect(110, 170, 401, 10))
         self.progressBar.setMaximum(0)
         self.progressBar.setProperty("value", -1)
         self.progressBar.setFormat("")
@@ -127,7 +131,7 @@ class Ui_MainWindow(object):
         self.statusbar = QtWidgets.QStatusBar(MainWindow)
         self.statusbar.setObjectName("statusbar")
         MainWindow.setStatusBar(self.statusbar)
-
+        self.restoreSettings()
         self.retranslateUi(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
@@ -145,8 +149,8 @@ class Ui_MainWindow(object):
         self.ipAddressCB.setPlaceholderText(_translate("MainWindow", "IP Address"))
 
     def startBtnClick(self):
-        ipAddress = self.ipAddressTB.text()
-        if ipAddress == "":
+        self.ipAddress = self.ipAddressTB.text()
+        if self.ipAddress == "":
             msg = QMessageBox()
             msg.setWindowTitle("Invalid IP")
             msg.setText("Invalid IP entered, please try again!")
@@ -179,7 +183,7 @@ class Ui_MainWindow(object):
         app.processEvents()
         adb.start_server()
         devices = adb.get_devices()
-        adbConnectionAttempt = adb.connect_remote(ipAddress, 5555)
+        adbConnectionAttempt = adb.connect_remote(self.ipAddress, 5555)
         if "failed" in adbConnectionAttempt:
             msg = QMessageBox()
             msg.setWindowTitle("Connection to IP Failed")
@@ -192,12 +196,13 @@ class Ui_MainWindow(object):
         self.stopBtn.setDisabled(False)
         self.progressBar.setVisible(True)
         app.processEvents()
-        if self.clearPrevLogsChB.isChecked():
+        self.clearPrevLog = self.clearPrevLogsChB.isChecked()
+        if self.clearPrevLogs:
             self.status.setText("Clearing Prev Logs")
             app.processEvents()
             adb.get_logcat("-c")
         self.isLogging = True
-        self.status.setText("Logging IP Address: " + ipAddress)
+        self.status.setText("Logging IP Address: " + self.ipAddress)
         self.t = TTT()
         self.t.fileName = self.fileName
         self.t.start()
@@ -214,7 +219,25 @@ class Ui_MainWindow(object):
             self.status.setText("Logging complete, file saved")
             self.t.quit_flag = True
             self.t.wait()
+            self.saveSettings()
+        self.stopBtn.setDisabled(True)
+        self.startBtn.setDisabled(False)
 
+    def closeEvent(self, event):
+        self.stopBtnClick()
+        super(Ui_MainWindow, self).closeEvent(event)
+
+    def saveSettings(self):
+        settings = QSettings('verizon', 'adblogger')
+        settings.setValue('ipaddress', self.ipAddress)
+        settings.setValue('clearprevlogs', self.clearPrevLogs)
+
+    def restoreSettings(self):
+        settings = QSettings('verizon', 'adblogger')
+        self.ipAddress = settings.value('ipaddress', self.ipAddress)
+        self.clearPrevLogs = settings.value('clearprevlogs', self.clearPrevLogs)
+        self.ipAddressTB.setText(self.ipAddress)
+        self.clearPrevLogsChB.setChecked(bool(self.clearPrevLogs))
 
 class TTT(QThread):
     def __init__(self):
